0546b0aff020d29c30ca32deb5c7064cbd4d38e8
Kevin Wong <kkwokwai22@gmail.com>
2017-03-20 16:10:32 -0700

    added express and some route

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+var express = require('express')
+var app = express();
+var jsonData = {count: 12, message: 'hey'}
 
-var jsonData = {count: 12, message: 'hey'};
+
+app.get('/todos', function(req, res){
+  res.send(jsonData);
+})
+
+app.post('/todos', function(req, res){
+
+})
+
+
+
+app.listen(3000, function(){
+  console.log('listening on port 3000')
+})

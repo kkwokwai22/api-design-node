3310e0a76876f0dafe9a9b2ed03d808ac3462580
Kevin Wong <kkwokwai22@gmail.com>
2017-03-20 17:19:08 -0700

    added a route

--- server.js
+++ server.js
@@ -2,11 +2,28 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 var express = require('express')
+var fs = require('fs')
 var app = express();
 var jsonData = {count: 12, message: 'hey'}
 
+var lion = {
+  name: 'samba',
+  id: 1,
+  age: 12,
+  pride: 'im cool',
+  gender: 'male'
+}
 
-app.get('/todos', function(req, res){
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html', function(err, data){
+    if(err) {
+      return console.error(err);
+    }
+  })
+})
+
+app.get('/data', function(req, res){
   res.send(jsonData);
 })
 
